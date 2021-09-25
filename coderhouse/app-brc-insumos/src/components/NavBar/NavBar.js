0e4ae57a36d79ebfcea9b0ba2fd0bba37aafd0c9
Mariano Painefil <marianopainefil@gmail.com>
2021-09-25 15:19:28 -0300

    agregar navbar

---
+++ coderhouse/app-brc-insumos/src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+    return(
+        <ul>
+            <li>Opcion 1</li>
+            <li>Opcion 1</li>
+            <li>Opcion 1</li>
+            <li>Opcion 1</li>
+        </ul>
+    )
+}
+
+export default NavBar;
